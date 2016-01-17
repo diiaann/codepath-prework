a530ad51a7e59904984dd5709bb9efa5959b9eb2
Dian <diiaann@gmail.com>
2016-01-17 15:29:51 -0800

    codepath prework

---
+++ README.md
@@ -0,0 +1,14 @@
+Time:
+1hr30 basic video walkthrough
+2hr to do optional stuff, explore xcode
+
+Required:
+Calcuate the tip amount
+Calculate the tip based on a percentage
+Refresh the total when bill and percentage input is changed
+Clear out of keyboard on tap
+
+Optional:
+Animate in tip stuff when bill field contains stuff
+Hide tip stuff when bill field is empty
+Calculate tip total for more than one person
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,16 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipGroup: UIView!
+    
+    @IBOutlet weak var tipTotalOne: UILabel!
+    @IBOutlet weak var tipTotalTwo: UILabel!
+    @IBOutlet weak var tipTotalThree: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tipGroup.alpha = 0
+        billField.becomeFirstResponder()
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func onEditingChange(sender: AnyObject) {
+        if billField.text == "" {
+            UIView.animateWithDuration(0.4, animations: {
+                self.tipGroup.alpha = 0
+                self.billField.frame = CGRectMake(48, 269, 242, 30)
+            })
+        }
+        else {
+            UIView.animateWithDuration(0.4, animations: {
+                self.tipGroup.alpha = 1
+                self.billField.frame = CGRectMake(48, 57, 242, 30)
+            })
+        }
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tip = billAmount *  tipPercentage
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        tipTotalOne.text = String(format: "$%.2f", billAmount + tip)
+        tipTotalTwo.text = String(format: "$%.2f", (billAmount + tip)/2)
+        tipTotalThree.text = String(format: "$%.2f", (billAmount + tip)/3)
 
-
+    }
 }
 
